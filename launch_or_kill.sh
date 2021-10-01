79ed231e228b09d6f94a04b5b8c9a059f87ef5aa
Alex Nichol <unixpickle@gmail.com>
2021-10-01 00:23:33 -0400

    allow args to be passed to launch_or_kill.sh
    We want to be able to pass --gnome-center to this in a keyboard shortcut

--- launch_or_kill.sh
+++ launch_or_kill.sh
@@ -3,5 +3,5 @@
 if [ $(ps -u $USER u | grep popup_calc | wc -l) -ge 2 ]; then
     killall popup_calc
 else
-    ~/.local/share/popup_calc/popup_calc
+    ~/.local/share/popup_calc/popup_calc $@
 fi
